4f7875cef72f0837ecab16c2bc9a1e12a815f0b5
danielm-qva <jmastrapa96@gmail.com>
2021-08-09 15:03:51 -0400

    Add  Register

--- src/app/Services/auth-service.service.ts
+++ src/app/Services/auth-service.service.ts
@@ -1,47 +1,48 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { environment } from '../../environments/environment';
-import {HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { environment } from "../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthServiceService {
-
   private URL = environment.URL;
- 
+
   public header = {
-      "Content-Type": "application/json"
-    };
-  
-      constructor(private _router:Router  , private http:HttpClient){
-          }
-       
+    "Content-Type": "application/json",
+  };
 
-  isAutenticar(){
-   return true ? (localStorage.getItem('token') != null && localStorage.getItem('email') != null) : false;
-    }
+  constructor(private _router: Router, private http: HttpClient) {}
 
-
-  Autenticar(email:String  , token:String ){
-       localStorage['email'] = email;
-       localStorage['token'] = token;
-        this._router.navigateByUrl('/app/dashboard');
+  isAutenticar() {
+    return true
+      ? localStorage.getItem("token") != null &&
+          localStorage.getItem("email") != null
+      : false;
   }
 
-  logout(){
-       localStorage.clear();
-       this._router.navigateByUrl('/login');
+  Autenticar(email: String, token: String) {
+    localStorage["email"] = email;
+    localStorage["token"] = token;
+    this._router.navigateByUrl("/app/dashboard");
+  }
+
+  logout() {
+    localStorage.clear();
+    this._router.navigateByUrl("/login");
   }
 
   //get Token
-   loggin(data:Object){
-     return  this.http.post(this.URL+'users/login' , data , {headers:this.header}); 
-   }
+  loggin(data: Object) {
+    return this.http.post(this.URL + "users/login", data, {
+      headers: this.header,
+    });
+  }
 
-    //{ "user" : { "email" :user.email , "password" : user.pass}});
-    Register_user(data:Object):Observable<any>{
-        return this.http.post(this.URL+'users/register' , data) ;
-    }
+  //{ "user" : { "email" :user.email , "password" : user.pass}});
+  Register_user(data: Object): Observable<any> {
+    return this.http.post(this.URL + "users/register", data);
+  }
 }
--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { AuthServiceService } from "../../Services/auth-service.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: 'register.component.html'
+  selector: "app-dashboard",
+  templateUrl: "register.component.html",
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  fromRegis = new FormGroup({
+    email: new FormControl(""),
+    pass: new FormControl("", [Validators.required, Validators.minLength(11)]),
+    pass2: new FormControl(),
+  });
+  load: boolean;
+  constructor(
+    public services: AuthServiceService,
+    private ruta: Router,
+    public Toas: ToastrService
+  ) {}
 
-  constructor() { }
+  atras() {
+    history.back();
+  }
 
+  ngOnInit(): void {
+    setInterval(() => {
+      this.confirmarPass();
+    }, 100);
+  }
+
+  confirmarPass() {
+    if (this.fromRegis.value.pass == this.fromRegis.value.pass2) return false;
+    else return true;
+  }
+
+  onSubmit() {
+    this.load = true;
+    let data: Object = {
+      user: {
+        email: this.fromRegis.controls.email.value,
+        password: this.fromRegis.controls.pass.value,
+      },
+    };
+
+    this.services.Register_user(data).subscribe(
+      (res) => {
+        this.Toas.success("Registro con exito..", "Boot Mvp", {
+          timeOut: 800,
+        });
+        setTimeout(() => {
+          this.load = false;
+        }, 500);
+        history.back();
+      },
+      (error) => {
+        this.Toas.error("Ha ocurrido un error..", "Boot Mvp", {
+          timeOut: 900,
+        });
+        setTimeout(() => {
+          this.load = false;
+        }, 500);
+      }
+    );
+  }
 }
